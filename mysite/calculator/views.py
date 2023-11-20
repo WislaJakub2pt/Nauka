5ac89949e98e761697a4af69b872b1973a41f984
WislaJakub2pt <jakub.wisla@vp.pl>
2023-11-20 10:28:27 -0500

    1.1.5

--- mysite/calculator/views.py
+++ mysite/calculator/views.py
@@ -1,8 +1,12 @@
 import numbers
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 def hello(request , number):
     return HttpResponse(f"Hello, World! {number}")
+
+@csrf_exempt
 def calc(request):
     pass
