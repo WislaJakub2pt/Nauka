fca7b2099355d328d2e707c658de9b80f12b585d
WislaJakub2pt <jakub.wisla@vp.pl>
2023-11-27 09:17:03 -0500

    1.1.6

--- mysite/calculator/views.py
+++ mysite/calculator/views.py
@@ -1,3 +1,4 @@
+import json
 import numbers
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -9,4 +10,7 @@ def hello(request , number):
 
 @csrf_exempt
 def calc(request):
-    pass
+    data = json.loads(request.body)
+    if data["operation"] == "+":
+        result = data["a"] + data["b"]
+        return HttpResponse(f"{result}")
