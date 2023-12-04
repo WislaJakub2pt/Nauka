72ad5f73d098f68a335eb99089109d3f00f8389d
WislaJakub2pt <jakub.wisla@vp.pl>
2023-12-04 07:35:45 -0500

    1.1.81

--- mysite/calculator/models.py
+++ mysite/calculator/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
+    login_count = models.IntegerField(default=0)
 
     def __string__(self):
         return f"{self.username} - {self.id}"   
